3265fa179012de7da3e6ef164d1df7b6b3084c53
Hugo M <alves.mh8@gmail.com>
2025-09-08 21:04:02 +0800

    image path fix

--- src/tutorials/probability/foundations/Foundations.jsx
+++ src/tutorials/probability/foundations/Foundations.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import mdSource from "./foundations.md?raw";
 import { Link } from "react-router-dom";
 import AppHeaderMini from "../../shell/components/AppHeaderMini";
